59d8f4a314fae87185c9d743904fdae8d526cc7e
MatoiDeCode <162004185+MatoiDeCode@users.noreply.github.com>
2024-05-21 09:17:58 +0700

    feat: navbar

---
+++ src/js/Script.js
@@ -0,0 +1,20 @@
+// Dropdown
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow");
+
+const toggleDropdown = () => {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("rotate");
+};
+
+dropdownBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+document.documentElement.addEventListener("click", () => {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
+  }
+});
