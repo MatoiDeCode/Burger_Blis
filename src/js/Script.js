ba45f087e01356aa8c99f5bc5166aa3c26cd8f53
MatoiDeCode <162004185+MatoiDeCode@users.noreply.github.com>
2024-05-28 13:09:52 +0700

    final: layout & style

--- src/js/Script.js
+++ src/js/Script.js
@@ -19,3 +19,20 @@ document.documentElement.addEventListener("click", () => {
   }
 });
 
+// Maps
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".sidebar-button");
+  const content = document.querySelector(".content");
+  const slider = document.querySelector(".slider");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const url = button.getAttribute("data-url");
+      if (url) {
+        window.location.href = url;
+      } else {
+        console.error("URL not found on button");
+      }
+    });
+  });
+});
